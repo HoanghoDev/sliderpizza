b9e1f8c50e3640dece5768b1ad39b3c365df5d7c
unknown <hohoang.dev@gmail.com>
2023-03-02 11:58:45 +0700

    first commit

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ Free code HTML CSS Javascript and Free learning web developer
 
 ![Logo](https://yt3.googleusercontent.com/LnD0yL5NAb8yvZu2d25qLZ-oAehUISz9tfe3aN36syGqTKbs4irbPeVUJfNlmVFRzel7KHV3-uo=s88-c-k-c0x00ffffff-no-rj)
 
+## Image in project
 
+![Alt text](project.png "Lun Dev") 
+- [Detailed instructions on this project](https://www.lundevweb.com/2023/01/tabs-slider-voi-html-css-va-javascript.html)
 
 
 ## Follow me for more free codes
 
- - [Youtube Lun Dev](https://www.youtube.com/results?search_query=l%C3%B9n+dev)
+ - [Youtube Lun Dev](https://www.youtube.com/results?search_query=lun+dev)
  - [Tiktok Lun Dev](https://www.tiktok.com/@lun.dev)
  - [Website Lun Dev Web](https://lundevweb.com)
 
@@ -20,7 +23,7 @@ Free code HTML CSS Javascript and Free learning web developer
 
 This project is built and shared by
 
-- Lun Dev
+- Lun Dev Youtube
 
 
 ## Feedback
--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-let items = document.querySelectorAll('.allowActive');
-let containerFuild = document.querySelector('.container-fuild');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let image = document.querySelector('.images');
+let items = document.querySelectorAll('.images .item');
+let contents = document.querySelectorAll('.content .item');
 
-containerFuild.addEventListener('scroll', ()=>{
-    items.forEach(item => {
-        if(item.offsetTop - containerFuild.scrollTop < window.innerHeight*(3/4)){
-            item.classList.add('active');
+let rotate = 0;
+let active = 0;
+let countItem = items.length;
+let rotateAdd = 360 / countItem;
+
+function nextSlider(){
+    active = active + 1 > countItem - 1 ? 0 : active + 1;
+    rotate = rotate + rotateAdd; 
+    show();
+}
+function prevSlider(){
+    active = active - 1 < 0 ? countItem - 1 : active - 1;
+    rotate = rotate - rotateAdd; 
+    show();     
+     
+}
+function show(){
+    image.style.setProperty("--rotate", rotate + 'deg');
+    image.style.setProperty("--rotate", rotate + 'deg');
+    contents.forEach((content, key) => {
+        if(key == active){
+            content.classList.add('active');
         }else{
-            item.classList.remove('active');
+            content.classList.remove('active');
         }
     })
-})
+}
+next.onclick = nextSlider;
+prev.onclick = prevSlider;
+// const autoNext = setInterval(nextSlider, 3000);
